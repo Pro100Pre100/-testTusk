d805dda0c75ce696703e136f02ea5d2d895bdabc
Pro100Pre100 <46046130+Pro100Pre100@users.noreply.github.com>
2025-03-16 18:12:49 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import SeminarCard from './components/SeminarCard'
 import { getSeminars } from './api/seminars'
 import { seminarsState } from './interfaces/seminars'
-import { CircularProgress, Grid2 } from '@mui/material'
+import { Alert, CircularProgress, Grid2 } from '@mui/material'
 import DeleteModal from './components/DeleteModal'
 import { useNavigate } from 'react-router-dom'
 import EditModal from './components/EditModal'
@@ -13,6 +13,7 @@ function App() {
   const [seminars, setSeminars] = useState<seminarsState[]>([]);
   const [deleteModal, setDeleteModal] = useState<boolean>(false);
   const [editModal, setEditModal] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
 
   const [updateSeminars, setUpdateSeminars] = useState<boolean>(true);
 
@@ -38,15 +39,13 @@ function App() {
       }
     }
     catch (error) {
-      setUpdateSeminars(false);
+      setError(true);
+      console.error(error);
     }
     finally {
       setUpdateSeminars(false);
     }
-
   }, [updateSeminars])
-
-  //reset the state after update the page
 
   window.onbeforeunload = () => {
     navigate('/', { state: null })
@@ -78,34 +77,35 @@ function App() {
 
   return (
     <div className='seminarContainer'>
-      {!seminars.length
+      {error
         ? <CircularProgress />
-        :
-        <>
-          <DeleteModal open={deleteModal} handleClose={closeModals} enableUpdateSeminars={enableUpdateSeminars} />
-          <EditModal open={editModal} handleClose={closeModals} seminars={seminars} enableUpdateSeminars={enableUpdateSeminars} />
-          <Grid2 container spacing={2}
-            sx={{
-              display: "grid",
-              overflowY: 'auto',
-              width: '95%',
-              height: '95%',
-              border: '2px solid black',
-              borderRadius: '10px',
-              padding: '10px',
-              gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
-            }}>
-            {seminars.map(({ id, photo, title, description }, index) =>
-              <SeminarCard
-                key={id}
-                img={photo}
-                title={title}
-                description={description}
-                handleDelete={openDeleteModal(id, index)}
-                handleEdit={openEditModal(id, index)}
-              />)}
-          </Grid2>
-        </>}
+        : seminars.length
+          ? <>
+            <DeleteModal open={deleteModal} handleClose={closeModals} enableUpdateSeminars={enableUpdateSeminars} />
+            <EditModal open={editModal} handleClose={closeModals} seminars={seminars} enableUpdateSeminars={enableUpdateSeminars} />
+            <Grid2 container spacing={2}
+              sx={{
+                display: "grid",
+                overflowY: 'auto',
+                width: '95%',
+                height: '95%',
+                border: '2px solid black',
+                borderRadius: '10px',
+                padding: '10px',
+                gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+              }}>
+              {seminars.map(({ id, photo, title, description }, index) =>
+                <SeminarCard
+                  key={id}
+                  img={photo}
+                  title={title}
+                  description={description}
+                  handleDelete={openDeleteModal(id, index)}
+                  handleEdit={openEditModal(id, index)}
+                />)}
+            </Grid2>
+          </>
+          : <Alert severity="error">Error Error on server side</Alert>}
     </div>
   )
 }
